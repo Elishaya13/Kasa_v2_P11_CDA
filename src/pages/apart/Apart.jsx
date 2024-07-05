aae669a672eb7d74bb4564b3c5c3b07ac63a35f7
Elishaya13 <a.lartillier@gmail.com>
2024-07-05 18:15:21 +0200

    style: add red color to host name and adjust width to ensure two-line display
    - Changed the host name color to red on the host card
    - Adjusted the width of the <p> element containing the host name to ensure it always displays on two lines

--- src/pages/apart/Apart.jsx
+++ src/pages/apart/Apart.jsx
@@ -26,46 +26,46 @@ const Apart = () => {
       .catch((e) => setError(e));
   }, [apartId]);
 
-  // Build the view only if the values ​​have been retrieved and initialized in "apart" state
+  // Build the view only if the values have been retrieved and initialized in "apart" state
   if (apart) {
     return (
-      <div className="apart">
-        <div className="apart__slideshow">
+      <div className='apart'>
+        <div className='apart__slideshow'>
           <Slideshow pictures={apart.pictures} alt={apart.description} />
         </div>
 
-        <div className="apart__profil">
-          <div className="apart__profil__name">
-            <div className="apart__profil__name__title">
+        <div className='apart__profil'>
+          <div className='apart__profil__name'>
+            <div className='apart__profil__name__title'>
               <h2>{apart.title}</h2>
               <p>{apart.location}</p>
             </div>
 
-            <div className="apart__profil__name__tags">
+            <div className='apart__profil__name__tags'>
               {apart.tags.map((item, index) => (
                 <TagLocation key={index} tags={item} />
               ))}
             </div>
           </div>
 
-          <div className="apart__profil__host">
+          <div className='apart__profil__host'>
             <Rating rating={apart.rating} />
 
-            <div className="apart__profil__host__hoster">
-              <p>{apart.host["name"]} </p>
-              <img src={apart.host["picture"]} alt={"proprietaire"}></img>
+            <div className='apart__profil__host__hoster'>
+              <p>{apart.host['name']} </p>
+              <img src={apart.host['picture']} alt={'proprietaire'}></img>
             </div>
           </div>
         </div>
 
-        <div className="apart__collapse">
+        <div className='apart__collapse'>
           <Collapse
-            title="Description"
+            title='Description'
             arrow={arrow_close}
             text={apart.description}
           />
           <Collapse
-            title="Équipement"
+            title='Équipement'
             arrow={arrow_close}
             text={apart.equipments.map((equipement, index) => (
               <li key={index}>{equipement}</li>
@@ -75,7 +75,7 @@ const Apart = () => {
       </div>
     );
   } else if (error) {
-    return <Navigate to="/error" />;
+    return <Navigate to='/error' />;
   }
   // Call the Loader component while waiting for the data
   else {
