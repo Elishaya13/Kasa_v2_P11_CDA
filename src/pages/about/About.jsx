dadba665246a01d9b8b68202ced27c660d8dbe69
Elishaya13 <a.lartillier@gmail.com>
2024-07-08 16:35:50 +0200

    refactor(about): inline AboutBody component into About page for sipmicity

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,16 +1,42 @@
+import './about.scss';
 import Banner from '../../components/banner/Banner';
-import AboutBody from './aboutBody/AboutBody';
-import './about.scss'
-import { IMAGES} from '../../assets/images/images';
+import Collapse from '../../components/collapse/Collapse';
 
+import { IMAGES } from '../../assets/images/images';
+import { ABOUT_TXT } from '../../constants/aboutPageStrings';
+import arrow_close from '../../assets/images/arrow_down.svg';
 
 const About = () => {
-    return (
-        <div className='about'>
-            <Banner banner={IMAGES.BANNER_ABOUT} />
-            <AboutBody />
-        </div>
-    );
+  return (
+    <div className='about'>
+      <Banner banner={IMAGES.BANNER_ABOUT} />
+      <div className='about__body'>
+        <Collapse
+          arrow={arrow_close}
+          title={ABOUT_TXT.first.title}
+          text={ABOUT_TXT.first.text}
+        />
+
+        <Collapse
+          arrow={arrow_close}
+          title={ABOUT_TXT.second.title}
+          text={ABOUT_TXT.second.text}
+        />
+
+        <Collapse
+          arrow={arrow_close}
+          title={ABOUT_TXT.third.title}
+          text={ABOUT_TXT.third.text}
+        />
+
+        <Collapse
+          arrow={arrow_close}
+          title={ABOUT_TXT.fourth.title}
+          text={ABOUT_TXT.fourth.text}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default About;
