3c7606f9b1cc7193307366ac6509e66c46d2f4a5
Elishaya13 <a.lartillier@gmail.com>
2024-07-08 10:52:48 +0200

    feat: Add dynamic transition animation for collapsible content to adapt to variable heights

--- src/components/collapse/Collapse.jsx
+++ src/components/collapse/Collapse.jsx
@@ -1,30 +1,54 @@
-import { useState } from "react";
-import "./collapse.scss";
+import { useEffect, useRef, useState } from 'react';
+import './collapse.scss';
 
+/**
+ * Component for creating a collapsible content area.
+ *
+ * @param {Object} props - Component props
+ * @param {string} props.title - Title of the collapsible section
+ * @param {string} props.arrow - URL of the arrow image
+ * @param {string} props.text - Text content to be displayed inside the collapsible section
+ * @returns {JSX.Element} The Collapse component
+ */
 const Collapse = ({ title, arrow, text }) => {
   const [open, setOpen] = useState(false);
+  // Ref to access the collapsible content DOM element
+  const contentRef = useRef(null);
 
+  // Effect to dynamically adjust the height of the collapsible content for animation purposes
+  useEffect(() => {
+    if (open) {
+      // If open, set max-height to the scrollHeight to expand the content
+      contentRef.current.style.maxHeight = `${contentRef.current.scrollHeight}px`;
+    } else {
+      // If closed, remove the max-height style to collapse the content
+      contentRef.current.style.maxHeight = null;
+    }
+  }, [open]);
+
+  // Handler for click events to toggle the open state
   const handleClick = () => {
     setOpen(!open);
   };
 
   return (
-    <div className="collapse">
-      <div className="collapse__container" onClick={handleClick}>
+    <div className='collapse'>
+      <div className='collapse__container' onClick={handleClick}>
         <h3>{title}</h3>
         <img
           src={arrow}
-          alt="fleche"
-          className={open ? "arrow-up" : "arrow-down"}
+          alt='fleche'
+          className={open ? 'arrow-up' : 'arrow-down'}
         ></img>
       </div>
 
-      <div className="collapse__textContent">
-        {open && (
-          <div className="collapse__textContent_text">
-            <p>{text}</p>
-          </div>
-        )}
+      <div
+        className={`collapse__textContent ${open ? 'open' : ''}`}
+        ref={contentRef}
+      >
+        <div className='collapse__textContent_text'>
+          <p>{text}</p>
+        </div>
       </div>
     </div>
   );
