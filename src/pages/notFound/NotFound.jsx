c26b003fe96eef9c842ab5c6b15aee7bfa7d2cd6
Elishaya13 <a.lartillier@gmail.com>
2024-07-08 16:27:24 +0200

    refactor(notFound): inline NotFoundBody component for clarity

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,10 +1,19 @@
-import NotFoundBody from "./notFoundBody/NotFoundBody";
-import "./notFound.scss";
+import './notFound.scss';
+import { STRINGS } from '../../constants/uiStrings';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div className="error_container">
-      <NotFoundBody />
+    <div className='error_container'>
+      <div className='error_container__body'>
+        <p className='error_container__body__message_number'>404</p>
+        <p className='error_container__body__message_oups'>
+          {STRINGS.ERROR_MESSAGE_404}
+        </p>
+        <p className='error_container__body__message_back'>
+          <Link to='/home'>{STRINGS.ERROR_MESSAGE_404_BACK_LINK}</Link>
+        </p>
+      </div>
     </div>
   );
 };
